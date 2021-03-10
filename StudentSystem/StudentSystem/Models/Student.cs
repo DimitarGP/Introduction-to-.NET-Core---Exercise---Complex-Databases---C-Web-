9e70dcbf28151918ed1505c826bdcb4967569185
Dimitar Petkov <petkov.dg@gmail.com>
2021-03-10 23:55:25 +0200

    Change name

--- StudentSystem/StudentSystem/Models/Student.cs
+++ StudentSystem/StudentSystem/Models/Student.cs
@@ -18,6 +18,8 @@ namespace StudentSystem.Models
         [Required]
         public string FirstName { get; set; }
 
+        public string SecondName { get; set; }
+
         public string LastName { get; set; }
 
         public string PhoneNumber { get; set; }
